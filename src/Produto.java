9374b4b1c782374bf4dbb3f3ea9b1283bf492822
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-16 14:47:14 -0300

    adicionando dadods na classe Produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,11 @@
 public class Produto {
+    public String codigo;
+    public double precoUnitario;
+    public int quantidade;
+
+    public Produto(String codigo, double precoUnitario, int quantidade) {
+        this.codigo = codigo;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
 }
